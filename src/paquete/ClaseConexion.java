99e283c0efde7b8ed95113be509c04e72696a80d
D4Guz <146863049+DaGuz06@users.noreply.github.com>
2025-05-12 12:48:08 +0200

    cambios en la conexion a MySQL

--- src/paquete/ClaseConexion.java
+++ src/paquete/ClaseConexion.java
@@ -3,9 +3,9 @@ package paquete;
 public class ClaseConexion {
 	
 	//se usa static para no estar creando instancias y que se pueda usar de forma global en toda la aplicacion
-    public static ConexionPostgreSQL conexion = new ConexionPostgreSQL(
-        /*usuario*/"neondb_owner",
-        /*contraseña*/"npg_XfWl7CHkgc0j",
-        /*nombre bbdd*/"neondb"
+    public static ConexionMySQL conexion = new ConexionMySQL(
+        /*usuario*/"root",
+        /*contraseña*/"",
+        /*nombre bbdd*/"test"
     );
 }
